cd5d822d4feb4247c2f1ad6e0d3b95aa0f59dd3c
palmala <palmailaszloprivate@gmail.com>
2023-03-12 17:30:23 +0100

    Converting to FastAPI

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
-from flask import Flask, Response
-from prometheus_client import Counter, generate_latest
+import uvicorn
+from fastapi import FastAPI, Response
+from fastapi.templating import Jinja2Templates
 import logging
+from prometheus_client import Counter, generate_latest
 
 logger = logging.getLogger(__name__)
-app = Flask(__name__)
 CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 TEST_COUNTER = Counter(
     'test_counter',
@@ -12,16 +16,16 @@ TEST_COUNTER = Counter(
 )
 
 
-@app.route('/metrics', methods=['GET'])
-def get_data():
-    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
-
-
-@app.route('/', methods=['GET'])
-def hello_page():
+@app.get(path="/")
+async def root():
     TEST_COUNTER.inc()
-    return "{'status': 'success', 'message': 'Hello World!'}"
+    return {"message": "Hello"}
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+@app.get(path="/metrics")
+async def root():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
