606075531db469bb17dbc03d8ba3259b2f0ff4f6
palmala <palmailaszloprivate@gmail.com>
2023-03-12 12:51:10 +0100

    Adding basic docker example

--- app/main.py
+++ app/main.py
@@ -1,25 +1,12 @@
 from flask import Flask, Response
-from prometheus_client import Counter, generate_latest
 import logging
 
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
-CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
-
-TEST_COUNTER = Counter(
-    'test_counter',
-    'A test counter'
-)
-
-
-@app.route('/metrics', methods=['GET'])
-def get_data():
-    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 
 @app.route('/', methods=['GET'])
 def hello_page():
-    TEST_COUNTER.inc()
     return "{'status': 'success', 'message': 'Hello World!'}"
 
 
